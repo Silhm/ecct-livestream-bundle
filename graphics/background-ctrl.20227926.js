ac7d3c7e3c5922a28bc109f10e9b196666719471
Simon <mail@simon-florentin.fr>
2020-05-14 17:39:12 +0200

    updated compiled elements

--- graphics/background-ctrl.20227926.js
+++ graphics/background-ctrl.20227926.js
@@ -67520,7 +67520,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40973" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35805" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
